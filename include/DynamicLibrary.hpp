7a84caabf623f466487653445ccaa04f9f9ec7e8
Morgan <caron.morgan91@gmail.com>
2020-02-04 12:09:46 +0100

    Fix CI

--- include/DynamicLibrary.hpp
+++ include/DynamicLibrary.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <sstream>
-#include <iostream>
 #include <string>
+#include <sstream>
+#include <utility>
+#include <iostream>
 
 #ifdef _WIN32
 # include <windows.h>
@@ -10,83 +11,85 @@
 # include <dlfcn.h>
 #endif
 
-class DynamicLibrary
+namespace CppUtils
 {
-public:
-#ifdef _WIN32
-	static constexpr const char* ext = ".dll";
-#elif defined(__linux__)
-	static constexpr const char* ext = ".so";
-#endif
-
-	DynamicLibrary() = default;
-	DynamicLibrary(const DynamicLibrary&) = delete;
-	DynamicLibrary(DynamicLibrary&& src) noexcept : m_library(std::exchange(src.m_library, nullptr))
-	{};
-	DynamicLibrary& operator=(const DynamicLibrary&) = delete;
-	DynamicLibrary& operator=(DynamicLibrary&& rhs) noexcept
+	class DynamicLibrary
 	{
-		m_library = std::exchange(rhs.m_library, nullptr);
-		return *this;
-	};
+	public:
+	#ifdef _WIN32
+		static constexpr const char* ext = ".dll";
+	#elif defined(__linux__)
+		static constexpr const char* ext = ".so";
+	#endif
 
-	DynamicLibrary(std::string_view path)
-	{
-		open(path);
-	};
-	virtual ~DynamicLibrary() noexcept
-	{
-		close();
-	};
-
-#ifdef _WIN32
-
-	template<typename Signature>
-	Signature getSymbol(std::string_view name)
-	{
-		auto oldMode = ::SetErrorMode(SEM_FAILCRITICALERRORS);
-		static_cast<void>(::GetLastError());
-		auto symbol = reinterpret_cast<Signature>(::GetProcAddress(static_cast<HMODULE>(m_library), name.data()));
-		auto error = ::GetLastError();
-		::SetErrorMode(oldMode);
-		if (error != ERROR_SUCCESS && !symbol)
+		DynamicLibrary() = default;
+		DynamicLibrary(const DynamicLibrary&) = delete;
+		DynamicLibrary(DynamicLibrary&& src) noexcept : m_library(std::exchange(src.m_library, nullptr))
+		{};
+		DynamicLibrary& operator=(const DynamicLibrary&) = delete;
+		DynamicLibrary& operator=(DynamicLibrary&& rhs) noexcept
 		{
-			std::stringstream stream;
-			stream << "Couldn't load function [" << name.data() << "]: ";
-			stream << "Error code: " << std::hex << error;
-			throw std::runtime_error(stream.str());
-		}
-		return symbol;
-	};
+			m_library = std::exchange(rhs.m_library, nullptr);
+			return *this;
+		};
 
-#elif defined(__linux__)
-
-	template<typename Signature>
-	Signature getSymbol(std::string_view name)
-	{
-		dlerror();
-		auto symbol = reinterpret_cast<Signature>(dlsym(m_library, name.data()));
-		auto error = dlerror();
-		if (error && !symbol)
+		DynamicLibrary(std::string_view path)
 		{
-			std::stringstream stream;
-			stream << "Couldn't load symbol [" << name.data() << "]: ";
-			stream << "Error code: " << std::hex << std::string(error);
-			throw std::runtime_error(stream.str());
+			open(path);
+		};
+		virtual ~DynamicLibrary() noexcept
+		{
+			close();
+		};
+
+	#ifdef _WIN32
+
+		template<typename Signature>
+		Signature getSymbol(std::string_view name)
+		{
+			auto oldMode = ::SetErrorMode(SEM_FAILCRITICALERRORS);
+			static_cast<void>(::GetLastError());
+			auto symbol = reinterpret_cast<Signature>(::GetProcAddress(static_cast<HMODULE>(m_library), name.data()));
+			auto error = ::GetLastError();
+			::SetErrorMode(oldMode);
+			if (error != ERROR_SUCCESS && !symbol)
+			{
+				std::stringstream stream;
+				stream << "Couldn't load function [" << name.data() << "]: ";
+				stream << "Error code: " << std::hex << error;
+				throw std::runtime_error(stream.str());
+			}
+			return symbol;
 		}
-		return symbol;
+
+	#elif defined(__linux__)
+
+		template<typename Signature>
+		Signature getSymbol(std::string_view name)
+		{
+			dlerror();
+			auto symbol = reinterpret_cast<Signature>(dlsym(m_library, name.data()));
+			auto error = dlerror();
+			if (error && !symbol)
+			{
+				std::stringstream stream;
+				stream << "Couldn't load symbol [" << name.data() << "]: ";
+				stream << "Error code: " << std::hex << std::string(error);
+				throw std::runtime_error(stream.str());
+			}
+			return symbol;
+		}
+
+	#endif
+
+	private:
+		void open(std::string_view path);
+		void close() noexcept;
+
+	#ifdef _WIN32
+		HMODULE m_library = nullptr;
+	#elif defined(__linux__)
+		void* m_library = nullptr;
+	#endif
 	};
-
-#endif
-	
-
-private:
-	void open(std::string_view path);
-	void close() noexcept;
-
-#ifdef _WIN32
-	HMODULE m_library = nullptr;
-#elif defined(__linux__)
-	void* m_library = nullptr;
-#endif
-};
+}
--- src/DynamicLibrary.cpp
+++ src/DynamicLibrary.cpp
@@ -1,63 +1,65 @@
 #include "DynamicLibrary.hpp"
 
-#ifdef _WIN32
-
-void DynamicLibrary::open(std::string_view path)
+namespace CppUtils
 {
-	m_library = ::LoadLibraryA(path.data());
-	if (!m_library)
+	#ifdef _WIN32
+
+	void DynamicLibrary::open(std::string_view path)
 	{
-		std::stringstream stream;
-		stream << "Couldn't load dynamic library at [" << path.data() << "]: ";
-		stream << "Error code: " << std::hex << GetLastError();
-		throw std::runtime_error(stream.str());
+		m_library = ::LoadLibraryA(path.data());
+		if (!m_library)
+		{
+			std::stringstream stream;
+			stream << "Couldn't load dynamic library at [" << path.data() << "]: ";
+			stream << "Error code: " << std::hex << GetLastError();
+			throw std::runtime_error(stream.str());
+		}
 	}
+
+	void DynamicLibrary::close() noexcept
+	{
+		if (!m_library)
+			return;
+		UINT oldMode = ::SetErrorMode(SEM_FAILCRITICALERRORS);
+
+		try
+		{
+			::FreeLibrary(static_cast<HMODULE>(m_library));
+		}
+		catch (const std::exception&)
+		{
+			std::cerr << "Couldn't unload dynamic library" << std::endl;
+		}
+		::SetErrorMode(oldMode);
+		m_library = nullptr;
+	}
+
+	#elif defined(__linux__)
+
+	void DynamicLibrary::open(std::string_view path)
+	{
+		m_library = dlopen(path.data(), RTLD_LAZY);
+
+		if (!m_library)
+		{
+			std::stringstream stream;
+			stream << "Couldn't load dynamic library at [" << path.data() << "]: ";
+			stream << "Error code: " << std::hex << dlerror();
+			throw std::runtime_error(stream.str());
+		}
+	}
+
+	void DynamicLibrary::close() noexcept
+	{
+		try
+		{
+			dlclose(m_library);
+		}
+		catch (const std::exception&)
+		{
+			std::cerr << "Couldn't unload dynamic library" << std::endl;
+		}
+	}
+
+	#endif
 }
-
-void DynamicLibrary::close() noexcept
-{
-	if (!m_library)
-		return;
-	UINT oldMode = ::SetErrorMode(SEM_FAILCRITICALERRORS);
-
-	try
-	{
-		::FreeLibrary(static_cast<HMODULE>(m_library));
-	}
-	catch (const std::exception&)
-	{
-		std::cerr << "Couldn't unload dynamic library" << std::endl;
-	}
-	::SetErrorMode(oldMode);
-	m_library = nullptr;
-}
-
-#elif defined(__linux__)
-
-void DynamicLibrary::open()
-{
-	m_library = dlopen(m_path.c_str(), RTLD_LAZY);
-
-	if (!m_library)
-	{
-		std::stringstream stream;
-		stream << "Couldn't load dynamic library at [" << m_path << "]: ";
-		stream << "Error code: " << std::hex << dlerror();
-		std::cerr << stream.str() << std::endl;
-		throw std::runtime_error(stream.str());
-	}
-	}
-
-void DynamicLibrary::close() noexcept
-{
-	try
-	{
-		dlclose(m_library);
-	}
-	catch (const std::exception&)
-	{
-		std::cerr << "Couldn't unload dynamic library" << std::endl;
-	}
-}
-
-#endif
