c57cb42f4d500c843184ced6795e906bad150eb3
Morgan <caron.morgan91@gmail.com>
2020-02-05 02:22:25 +0100

    NonCopyable

--- include/Class/NonCopyable.hpp
+++ include/Class/NonCopyable.hpp
@@ -2,10 +2,15 @@
 
 namespace CppUtils::Class
 {
+	template<class T>
 	class NonCopyable
 	{
 	public:
 		NonCopyable(const NonCopyable&) = delete;
-		NonCopyable& operator=(const NonCopyable&) = delete;
+		T& operator=(const T&) = delete;
+
+	protected:
+		NonCopyable() = default;
+		~NonCopyable() = default;
 	};
 }
