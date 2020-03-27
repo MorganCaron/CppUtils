173412d8574db11ea2f294a4728694a4228a6607
Morgan <caron.morgan91@gmail.com>
2020-03-27 01:35:18 +0100

    Code cleaning

--- Terminal/include/Terminal/TextModifier.hpp
+++ Terminal/include/Terminal/TextModifier.hpp
@@ -7,66 +7,42 @@
 
 using namespace std::literals;
 
-namespace CppUtils::Terminal
+namespace CppUtils::Terminal::TextModifier
 {
-	class TextModifier
+	[[maybe_unused]] static constexpr const auto Reset = "\x1B[0m"sv;
+	[[maybe_unused]] static constexpr const auto EraseCurrentLine = "\x1B[2K0E"sv;
+
+	namespace TextStyle
 	{
-	public:
-		enum class SelectGraphicRenditionCode
-		{
-			TextStyle_Bolder = 1,
-			TextStyle_Lighter = 2,
-			TextStyle_Italic = 3,
-			TextStyle_Underline = 4,
-			TextColor_Black = 30,
-			TextColor_Red = 31,
-			TextColor_Green = 32,
-			TextColor_Yellow = 33,
-			TextColor_Blue = 34,
-			TextColor_Magenta = 35,
-			TextColor_Cyan = 36,
-			TextColor_White = 37,
-			TextColor_Default = 39,
-			BackgroundColor_Black = 40,
-			BackgroundColor_Red = 41,
-			BackgroundColor_Green = 42,
-			BackgroundColor_Yellow = 43,
-			BackgroundColor_Blue = 44,
-			BackgroundColor_Magenta = 45,
-			BackgroundColor_Cyan = 46,
-			BackgroundColor_White = 47,
-			BackgroundColor_Default = 49
-		};
-		using SGRCode = SelectGraphicRenditionCode;
-		
-		static constexpr const auto Esc = "\033"sv;
-		static constexpr const auto Reset = "\033c"sv;
-
-		explicit TextModifier(const std::vector<SGRCode>& codes)
-		{
-			std::ostringstream oss;
-			oss << Esc << '[';
-			for (std::size_t i = 0; i < codes.size(); ++i)
-			{
-				oss << static_cast<int>(codes[i]);
-				if (i > 0)
-					oss << ';';
-			}
-			oss << 'm';
-			m_AnsiEscapeCode = oss.str();
-		}
-
-		inline std::string getAnsiEscapeCode() const
-		{
-			return m_AnsiEscapeCode;
-		}
-
-	private:
-		std::string m_AnsiEscapeCode;
-	};
-
-	inline std::ostream& operator<<(std::ostream& os, const TextModifier& modifier)
+		[[maybe_unused]] static constexpr const auto Bolder = "\x1B[1m"sv;
+		[[maybe_unused]] static constexpr const auto Lighter = "\x1B[2m"sv;
+		[[maybe_unused]] static constexpr const auto Italic = "\x1B[3m"sv;
+		[[maybe_unused]] static constexpr const auto Underline = "\x1B[4m"sv;
+	}
+	
+	namespace TextColor
 	{
-		return os << modifier.getAnsiEscapeCode();
+		[[maybe_unused]] static constexpr const auto Black = "\x1B[30m"sv;
+		[[maybe_unused]] static constexpr const auto Red = "\x1B[31m"sv;
+		[[maybe_unused]] static constexpr const auto Green = "\x1B[32m"sv;
+		[[maybe_unused]] static constexpr const auto Yellow = "\x1B[33m"sv;
+		[[maybe_unused]] static constexpr const auto Blue = "\x1B[34m"sv;
+		[[maybe_unused]] static constexpr const auto Magenta = "\x1B[35m"sv;
+		[[maybe_unused]] static constexpr const auto Cyan = "\x1B[36m"sv;
+		[[maybe_unused]] static constexpr const auto White = "\x1B[37m"sv;
+		[[maybe_unused]] static constexpr const auto Default = "\x1B[39m"sv;
+	}
+
+	namespace BackgroundColor
+	{
+		[[maybe_unused]] static constexpr const auto Black = "\x1B[40m"sv;
+		[[maybe_unused]] static constexpr const auto Red = "\x1B[41m"sv;
+		[[maybe_unused]] static constexpr const auto Green = "\x1B[42m"sv;
+		[[maybe_unused]] static constexpr const auto Yellow = "\x1B[43m"sv;
+		[[maybe_unused]] static constexpr const auto Blue = "\x1B[44m"sv;
+		[[maybe_unused]] static constexpr const auto Magenta = "\x1B[45m"sv;
+		[[maybe_unused]] static constexpr const auto Cyan = "\x1B[46m"sv;
+		[[maybe_unused]] static constexpr const auto White = "\x1B[47m"sv;
+		[[maybe_unused]] static constexpr const auto Default = "\x1B[49m"sv;
 	}
 }
