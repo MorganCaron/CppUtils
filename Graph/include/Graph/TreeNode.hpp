7a8e05687ab61f2930ced36da787503b37d7ab16
Morgan <caron.morgan91@gmail.com>
2020-10-15 06:15:11 +0200

    Graph/treeNodeToString

--- Graph/include/Graph/TreeNode.hpp
+++ Graph/include/Graph/TreeNode.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <vector>
 
 namespace CppUtils::Graph
@@ -10,4 +11,21 @@ namespace CppUtils::Graph
 		Storage self;
 		std::vector<TreeNode<Storage>> childs = {};
 	};
+
+	std::string treeNodeToString(const TreeNode<std::string> treeNode, const std::string prefix = "") noexcept
+	{
+		auto output = " " + treeNode.self + '\n';
+		const auto nbChilds = treeNode.childs.size();
+
+		for (auto i = 0u; i < nbChilds; ++i)
+			output += prefix + " " + ((i != nbChilds - 1) ? "├" : "└") + "─" + treeNodeToString(treeNode.childs.at(i), prefix + ((i != nbChilds - 1) ? " |" : "  "));
+		return output;
+	}
+	/*
+	├ = 195
+	─ = 196
+	│ = 179
+	└ = 192
+	*/
+
 }
--- UnitTests/include/UnitTests/Graph/TreeNode.hpp
+++ UnitTests/include/UnitTests/Graph/TreeNode.hpp
@@ -7,14 +7,32 @@ namespace UnitTests::Graph::TreeNode
 	const auto tests = std::vector<CppUtils::Test::UnitTest>{
 
 		CppUtils::Test::UnitTest("Graph/TreeNode", [] {
-			using StringTreeNode = CppUtils::Graph::TreeNode<const std::string>;
+			using StringTreeNode = CppUtils::Graph::TreeNode<std::string>;
+			
 			auto root = StringTreeNode{"Root"};
-			root.childs.emplace_back(StringTreeNode{"Branch0"});
-			root.childs.emplace_back(StringTreeNode{"Branch1"});
 			CppUtils::Log::Logger::logInformation(root.self);
 			ASSERT(root.self == "Root");
+			
+			root.childs.emplace_back(StringTreeNode{"Branch0"});
+			root.childs.emplace_back(StringTreeNode{"Branch1"});
 			CppUtils::Log::Logger::logInformation(root.childs.at(1).self);
 			ASSERT(root.childs.at(1).self == "Branch1");
+
+			auto& branch0 = root.childs[0];
+			branch0.childs.emplace_back(StringTreeNode{"SubBranch0"});
+			branch0.childs.emplace_back(StringTreeNode{"SubBranch1"});
+			branch0.childs.emplace_back(StringTreeNode{"SubBranch2"});
+			ASSERT(branch0.childs.size() == 3);
+
+			root.childs.emplace_back(StringTreeNode{"Branch2"});
+			auto& branch2 = root.childs[2];
+			branch2.childs.emplace_back(StringTreeNode{"SubBranch0"});
+			auto& subBranch0 = branch2.childs[0];
+			subBranch0.childs.emplace_back(StringTreeNode{"SubSubBranch0"});
+			subBranch0.childs.emplace_back(StringTreeNode{"SubSubBranch1"});
+			root.childs.emplace_back(StringTreeNode{"Branch3"});
+			CppUtils::Terminal::setConsoleOutputUTF8();
+			CppUtils::Log::Logger::logDetail(CppUtils::Graph::treeNodeToString(root));
 		})
 
 	};
