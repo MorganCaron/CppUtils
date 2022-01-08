e18190547415e860a63c1450e80fa4d31d8f1a44
Morgan Caron <caron.morgan91@gmail.com>
2022-01-08 17:43:03 +0100

    Second Vector::merge function

--- Container/include/CppUtils/Container/Vector.hpp
+++ Container/include/CppUtils/Container/Vector.hpp
@@ -7,6 +7,13 @@
 namespace CppUtils::Container::Vector
 {
 	template<typename T>
+	inline void merge(std::vector<T>& dest, const std::vector<T>& src)
+	{
+		dest.reserve(dest.size() + src.size());
+		dest.insert(dest.end(), src.begin(), src.end());
+	}
+
+	template<typename T>
 	[[nodiscard]] std::vector<T> merge(const std::initializer_list<std::reference_wrapper<const std::vector<T>>>& vectors)
 	{
 		auto size = std::size_t{0};
