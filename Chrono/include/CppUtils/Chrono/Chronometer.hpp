a4c850498f2edf1d2c57becf7513716f3edfe56b
Morgan Caron <caron.morgan91@gmail.com>
2022-01-26 00:48:59 +0100

    Chronometer

--- Chrono/include/CppUtils/Chrono/Chronometer.hpp
+++ Chrono/include/CppUtils/Chrono/Chronometer.hpp
@@ -5,6 +5,39 @@
 
 namespace CppUtils::Chrono
 {
+	template <typename Rep, typename Period>
+	[[nodiscard]] std::string durationToString(std::chrono::duration<Rep, Period> duration)
+	{
+		const auto hour = std::chrono::duration_cast<std::chrono::hours>(duration);
+		duration -= hour;
+		const auto min = std::chrono::duration_cast<std::chrono::minutes>(duration);
+		duration -= min;
+		const auto sec = std::chrono::duration_cast<std::chrono::seconds>(duration);
+		duration -= sec;
+		const auto millisec = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+		duration -= millisec;
+		const auto microsec = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+		duration -= microsec;
+		const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+
+		auto str = std::string{};
+		if (hour.count() > 0)
+			str += " " + std::to_string(hour.count()) + "h";
+		if (min.count() > 0)
+			str += " " + std::to_string(min.count()) + "m";
+		if (sec.count() > 0)
+			str += " " + std::to_string(sec.count()) + "s";
+		if (millisec.count() > 0)
+			str += " " + std::to_string(millisec.count()) + "ms";
+		if (microsec.count() > 0)
+			str += " " + std::to_string(microsec.count()) + "us";
+		if (ns.count() > 0)
+			str += " " + std::to_string(ns.count()) + "ns";
+		return !str.empty() ? str.substr(1) : "0ns";
+	}
+
+	template<typename Clock = std::chrono::high_resolution_clock>
+	requires std::chrono::is_clock_v<Clock>
 	class Chronometer final
 	{
 	public:
@@ -13,54 +46,23 @@ namespace CppUtils::Chrono
 			start();
 		}
 
-		inline void start()
+		void start()
 		{
-			m_start = std::chrono::high_resolution_clock::now();
+			m_start = Clock::now();
 		}
 
-		inline void stop()
+		void stop()
 		{
-			m_end = std::chrono::high_resolution_clock::now();
+			m_end = Clock::now();
 		}
 
-		[[nodiscard]] inline auto getDuration() const
+		[[nodiscard]] auto getDuration() const
 		{
 			return m_end - m_start;
 		}
 
-		[[nodiscard]] inline std::string getText() const
-		{
-			auto duration = getDuration();
-			const auto hour = std::chrono::duration_cast<std::chrono::hours>(duration);
-			duration -= hour;
-			const auto min = std::chrono::duration_cast<std::chrono::minutes>(duration);
-			duration -= min;
-			const auto sec = std::chrono::duration_cast<std::chrono::seconds>(duration);
-			duration -= sec;
-			const auto millisec = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
-			duration -= millisec;
-			const auto microsec = std::chrono::duration_cast<std::chrono::microseconds>(duration);
-			duration -= microsec;
-			const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
-
-			auto str = std::string{};
-			if (hour.count() > 0)
-				str += " " + std::to_string(hour.count()) + "h";
-			if (min.count() > 0)
-				str += " " + std::to_string(min.count()) + "m";
-			if (sec.count() > 0)
-				str += " " + std::to_string(sec.count()) + "s";
-			if (millisec.count() > 0)
-				str += " " + std::to_string(millisec.count()) + "ms";
-			if (microsec.count() > 0)
-				str += " " + std::to_string(microsec.count()) + "us";
-			if (ns.count() > 0)
-				str += " " + std::to_string(ns.count()) + "ns";
-			return !str.empty() ? str.substr(1) : "0ns";
-		}
-
 	private:
-		std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
-		std::chrono::time_point<std::chrono::high_resolution_clock> m_end;
+		std::chrono::time_point<Clock> m_start;
+		std::chrono::time_point<Clock> m_end;
 	};
 }
