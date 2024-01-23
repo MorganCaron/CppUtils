299b94152b85cb1882db39158e8481f0320766d6
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-01-23 22:30:54 +0100

    hide implementation details of TextModifier class

--- Terminal/include/CppUtils/Terminal/TextModifier.hpp
+++ Terminal/include/CppUtils/Terminal/TextModifier.hpp
@@ -18,8 +18,8 @@ namespace CppUtils::Terminal
 #if defined(OS_LINUX) || defined(OS_MACOS)
 	namespace ANSIEscapeCode
 	{
-		[[maybe_unused]] static constexpr auto Reset = "\x1B[0m"sv;
-		[[maybe_unused]] static constexpr auto EraseCurrentLine = "\x1B[2K0E"sv;
+		[[maybe_unused]] inline constexpr auto Reset = "\x1B[0m"sv;
+		[[maybe_unused]] inline constexpr auto EraseCurrentLine = "\x1B[2K0E"sv;
 	}
 #endif
 
@@ -32,52 +32,16 @@ namespace CppUtils::Terminal
 			std::FILE* file,
 			TextColor::TextColorEnum textColor = TextColor::TextColorEnum::Default,
 			BackgroundColor::BackgroundColorEnum backgroundColor = BackgroundColor::BackgroundColorEnum::Default,
-			[[maybe_unused]] std::optional<TextStyle::TextStyleEnum> textStyle = std::nullopt):
-			m_modify{true},
-			m_file{file}
-		{
-			[[maybe_unused]] const auto text = TextColor::getTextColorCode(textColor);
-			[[maybe_unused]] const auto background = BackgroundColor::getBackgroundColorCode(backgroundColor);
-#if defined(OS_WINDOWS)
-			m_attributes = getTextColor(stream);
-			SetConsoleTextAttribute(getTerminalHandle(m_file), (background << 4) + text);
-#elif defined(OS_LINUX) || defined(OS_MACOS)
-			std::print(m_file, "{}{}", text, background);
-			if (textStyle)
-				std::print(m_file, "{}", TextStyle::getTextStyleCode(*textStyle));
-#endif
-		}
+			[[maybe_unused]] std::optional<TextStyle::TextStyleEnum> textStyle = std::nullopt);
 
 		TextModifier(TextModifier&& other) noexcept = default;
 		TextModifier& operator=(TextModifier&& other) noexcept = default;
 
-		~TextModifier()
-		{
-			if (!m_modify)
-				return;
-#if defined(OS_WINDOWS)
-			SetConsoleTextAttribute(Terminal::getTerminalHandle(m_file), m_attributes);
-#elif defined(OS_LINUX) || defined(OS_MACOS)
-			std::print(m_file, "{}", ANSIEscapeCode::Reset);
-#endif
-		}
+		~TextModifier();
 
 #if defined(OS_WINDOWS)
-		[[nodiscard]] static auto getTextColor(std::FILE* file) -> WORD
-		{
-			CONSOLE_SCREEN_BUFFER_INFO info;
-			if (!GetConsoleScreenBufferInfo(getTerminalHandle(file), &info))
-				return 0;
-			return info.wAttributes;
-		}
-
-		[[nodiscard]] static auto getTextColor(std::ostream& stream) -> WORD
-		{
-			CONSOLE_SCREEN_BUFFER_INFO info;
-			if (!GetConsoleScreenBufferInfo(getTerminalHandle(stream), &info))
-				return 0;
-			return info.wAttributes;
-		}
+		[[nodiscard]] static auto getTextColor(std::FILE* file) -> std::uint16_t;
+		[[nodiscard]] static auto getTextColor(std::ostream& stream) -> std::uint16_t;
 #endif
 
 		static auto eraseCurrentLine([[maybe_unused]]std::ostream& stream) -> void
@@ -91,7 +55,7 @@ namespace CppUtils::Terminal
 		bool m_modify = false;
 		std::FILE* m_file = nullptr;
 #if defined(OS_WINDOWS)
-		WORD m_attributes;
+    std::uint16_t m_attributes;
 #endif
 	};
 }
--- Terminal/src/TextModifier.cpp
+++ Terminal/src/TextModifier.cpp
@@ -1,8 +1,53 @@
 #include <CppUtils/Terminal/TextModifier.hpp>
 
+#include <windows.h>
+
 namespace CppUtils::Terminal
 {
+		TextModifier::TextModifier(
+			std::FILE* file,
+			TextColor::TextColorEnum textColor,
+			BackgroundColor::BackgroundColorEnum backgroundColor,
+			[[maybe_unused]] std::optional<TextStyle::TextStyleEnum> textStyle):
+			m_modify{true},
+			m_file{file}
+		{
+			[[maybe_unused]] const auto text = TextColor::getTextColorCode(textColor);
+			[[maybe_unused]] const auto background = BackgroundColor::getBackgroundColorCode(backgroundColor);
 #if defined(OS_WINDOWS)
-	WORD TextModifier::m_attributes{};
+			m_attributes = getTextColor(m_file);
+			SetConsoleTextAttribute(getTerminalHandle(m_file), static_cast<std::uint16_t>((background << 4) + text));
+#elif defined(OS_LINUX) || defined(OS_MACOS)
+			std::print(m_file, "{}{}", text, background);
+			if (textStyle)
+				std::print(m_file, "{}", TextStyle::getTextStyleCode(*textStyle));
 #endif
+		}
+
+		TextModifier::~TextModifier()
+		{
+			if (!m_modify)
+				return;
+#if defined(OS_WINDOWS)
+			SetConsoleTextAttribute(Terminal::getTerminalHandle(m_file), m_attributes);
+#elif defined(OS_LINUX) || defined(OS_MACOS)
+			std::print(m_file, "{}", ANSIEscapeCode::Reset);
+#endif
+		}
+
+		auto TextModifier::getTextColor(std::FILE* file) -> std::uint16_t
+		{
+			CONSOLE_SCREEN_BUFFER_INFO info;
+			if (!GetConsoleScreenBufferInfo(getTerminalHandle(file), &info))
+				return 0;
+			return info.wAttributes;
+		}
+
+		auto TextModifier::getTextColor(std::ostream& stream) -> std::uint16_t
+		{
+			CONSOLE_SCREEN_BUFFER_INFO info;
+			if (!GetConsoleScreenBufferInfo(getTerminalHandle(stream), &info))
+				return 0;
+			return info.wAttributes;
+		}
 }
