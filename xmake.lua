851937480c67ca54f36bd075ca6f8994c331cbe1
Arthur Laurent <arthur.laurent4@gmail.com>
2024-02-03 23:41:30 +0100

    Feature/module (#10)
    * fix Utility.cpp
    
    * fix xmake.lua
    
    * Apply clang-format on Utility.mpp
    
    * use <CppUtils/System/Windows.hpp> instead of <windows.h>
    
    * fix compilation on clang for windows
    
    * fix linking on windows platform
    
    * fix compilation with msvc
    
    * Revert "fix compilation with msvc"
    
    This reverts commit 47ea0e8bcdbc1cc4deeae789a05eb49ee569f9a6.
    
    * fix linking

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,14 @@ set_project("CppUtils")
 set_version("0.0.0", {build = "%Y%m%d%H%M"})
 set_license("LGPL3")
 set_languages("clatest", "cxxlatest")
-set_warnings("allextra", "error", "pedantic")
-add_cxflags("-Wconversion -Wno-error=deprecated-declarations -Wfatal-errors")
+set_warnings("allextra", "pedantic", "error")
+add_cxflags("clang::-Wno-error=deprecated-declarations")
 add_cxflags("clang::-fcolor-diagnostics", "clang::-fansi-escape-codes", "gcc::-fdiagnostics-color=always")
--- add_cxflags("-Wno-gnu-statement-expression-from-macro-expansion -Wno-gnu-statement-expression")
+-- add_cxflags("-Wno-gnu-statement-expression-from-macro-expansion -Wno-gnu-statement-expression", {tools = { "clang", "gcc" })
 set_optimize("fastest")
 
-set_policy("check.auto_ignore_flags", false)
-add_cxxflags("-fexperimental-library") -- Pour avoir std::jthread
+add_cxxflags("clang::-fexperimental-library", {force = true}) -- Pour avoir std::jthread
+add_cxxflags("cl::/EHsc", {force = true}) -- Pour avoir std::jthread
 
 add_rules(
 	"mode.debug",
