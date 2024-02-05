eeec4085eabb678a295b927980b6914f9b3a116d
Morgan Caron <caron.morgan91@gmail.com>
2024-02-05 22:35:33 +0100

    Répertoires src/include/modules/tests

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ set_license("LGPL3")
 set_languages("clatest", "cxxlatest")
 set_warnings("allextra", "pedantic", "error")
 add_cxflags("-Wconversion -Wfatal-errors")
-add_cxflags("clang::-Wno-error=deprecated-declarations")
+add_cxflags("clang::-Wno-deprecated-declarations")
 add_cxflags("clang::-fcolor-diagnostics", "clang::-fansi-escape-codes", "gcc::-fdiagnostics-color=always")
 -- add_cxflags("-Wno-gnu-statement-expression-from-macro-expansion -Wno-gnu-statement-expression", {tools = { "clang", "gcc" })
 set_optimize("fastest")
@@ -26,8 +26,24 @@ add_rules(
 	"mode.lsan",
 	"mode.ubsan")
 
-add_defines(is_kind("static") and "STATIC_LIB" or "SHARED_LIB")
+target("CppUtils")
+	set_kind("$(kind)")
+	
+	if is_plat("windows") and is_config("cxx", "cl") and not is_plat("mingw") then
+		set_runtimes(is_mode("debug") and "MDd" or "MD")
+		add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", { public = true })
+		add_cxflags("/wd4251", {force = true}) -- ‘identifier’ : class ‘type’ needs to have dll-interface to be used by clients of class ‘type2’
+	elseif is_plat("linux", "macosx") then
+		add_syslinks("pthread", "dl")
+	end
 
-includes("Sources")
+	set_policy("build.merge_archive", true)
+	set_policy("build.c++.modules", true)
+	add_defines("DLL_EXPORT")
+	add_defines(is_kind("static") and "STATIC_LIB" or "SHARED_LIB")
+	add_includedirs("include", { public = true })
+	add_files("modules/**.mpp", { public = true })
+	add_headerfiles("include/**.hpp")
 
-includes("UnitTests")
+
+includes("tests")
