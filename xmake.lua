25d56860fbdbcd2dcfb3046bab4644155272fc24
Morgan Caron <caron.morgan91@gmail.com>
2024-03-23 20:29:36 +0100

    Moving set_runtimes outside the target

--- xmake.lua
+++ xmake.lua
@@ -13,6 +13,14 @@ set_optimize("fastest")
 add_cxxflags("clang::-fexperimental-library", {force = true}) -- Pour avoir std::jthread
 add_cxxflags("cl::/EHsc", {force = true}) -- Pour avoir std::jthread
 
+if is_plat("windows") and is_config("cxx", "cl") and not is_plat("mingw") then
+	set_runtimes(is_mode("debug") and "MDd" or "MD")
+	add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", { public = true })
+	add_cxflags("/wd4251", {force = true}) -- ‘identifier’ : class ‘type’ needs to have dll-interface to be used by clients of class ‘type2’
+elseif is_plat("linux", "macosx") then
+	add_syslinks("pthread", "dl")
+end
+
 add_rules(
 	"mode.debug",
 	"mode.release",
@@ -31,19 +39,11 @@ option("enable_tests")
 
 target("CppUtils", function()
 	set_kind("moduleonly")
-	
-	if is_plat("windows") and is_config("cxx", "cl") and not is_plat("mingw") then
-		set_runtimes(is_mode("debug") and "MDd" or "MD")
-		add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", { public = true })
-		add_cxflags("/wd4251", {force = true}) -- ‘identifier’ : class ‘type’ needs to have dll-interface to be used by clients of class ‘type2’
-	elseif is_plat("linux", "macosx") then
-		add_syslinks("pthread", "dl")
-	end
 
-	set_policy("build.merge_archive", true)
 	set_policy("build.c++.modules", true)
 	add_defines("DLL_EXPORT")
 	add_defines(is_kind("static") and "STATIC_LIB" or "SHARED_LIB")
+
 	add_files("modules/**.mpp", { public = true })
 	add_includedirs("include", { public = true })
 	add_headerfiles("include/(CppUtils/**.hpp)")
