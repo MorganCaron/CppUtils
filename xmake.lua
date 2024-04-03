959850209f7b6cf7018a2f7e5d876d2125b37484
Morgan Caron <caron.morgan91@gmail.com>
2024-04-03 14:01:00 +0200

    Update CI

--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,7 @@ set_version("0.0.0", {build = "%Y%m%d%H%M"})
 set_license("LGPL3")
 set_languages("clatest", "cxxlatest")
 set_warnings("allextra", "pedantic", "error")
-set_policy("check.auto_ignore_flags", false)
-add_cxflags("-Wconversion -Wfatal-errors", {force = true})
-add_cxflags("clang::-Wno-deprecated-declarations -Wno-unknown-attributes")
+add_cxflags("clang::-Wconversion", "clang::-Wfatal-errors", "clang::-Wno-deprecated-declarations -Wno-unknown-attributes")
 add_cxflags("clang::-fcolor-diagnostics", "clang::-fansi-escape-codes", "gcc::-fdiagnostics-color=always")
 -- add_cxflags("-Wno-gnu-statement-expression-from-macro-expansion -Wno-gnu-statement-expression", {tools = { "clang", "gcc" })
 set_optimize("fastest")
@@ -13,9 +11,9 @@ set_optimize("fastest")
 add_cxxflags("clang::-fexperimental-library", {force = true}) -- Pour avoir std::jthread
 add_cxxflags("cl::/EHsc", {force = true}) -- Pour avoir std::jthread
 
-if is_plat("windows") and is_config("cxx", "cl") and not is_plat("mingw") then
+if is_plat("windows") and not is_plat("mingw") then
 	set_runtimes(is_mode("debug") and "MDd" or "MD")
-	add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", { public = true })
+	add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS", { public = true })
 	add_cxflags("/wd4251", {force = true}) -- ‘identifier’ : class ‘type’ needs to have dll-interface to be used by clients of class ‘type2’
 elseif is_plat("linux", "macosx") then
 	add_syslinks("pthread", "dl")
