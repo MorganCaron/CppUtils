d0c52f4ab94e53ef04c834408557c43254ab5c48
Morgan Caron <caron.morgan91@gmail.com>
2026-07-26 15:28:21 +0200

    Try expected

--- include/CppUtils/Error.hpp
+++ include/CppUtils/Error.hpp
@@ -2,10 +2,34 @@
 
 #include <expected>
 
-#define TRY(expected) \
-	({ \
-		auto&& result = expected; \
-		if (not result.has_value()) [[unlikely]] \
-			return std::unexpected{result.error()}; \
-		result.value(); \
-	})
+#define UNWRAP_OR_RETURN(variable, expression) \
+	auto result = (expression); \
+	if (not result) \
+		return; \
+	auto& variable = *result
+
+#define UNWRAP_OR_UNEXPECTED(variable, expression) \
+	auto result = (expression); \
+	if (not result) \
+		return std::unexpected(result.error()); \
+	auto& variable = *result
+
+#if defined(__GNUC__) or defined(__clang__)
+#	define TRY(expected) \
+		({ \
+			auto&& result = expected; \
+			if (not result.has_value()) [[unlikely]] \
+				return std::unexpected{result.error()}; \
+			result.value(); \
+		})
+
+#	define TRY_VOID(expected) \
+		({ \
+			auto&& result = expected; \
+			if (not result.has_value()) [[unlikely]] \
+				return; \
+			result.value(); \
+		})
+
+#	define TRY_OR_RETURN(expected) TRY_VOID(expected)
+#endif
